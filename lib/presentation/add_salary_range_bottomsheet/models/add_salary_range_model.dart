70b36468204c15eff84ef805e7e53253da729719
Inaam Ul Haq <inaamulhaq7847@gmail.com>
2024-01-11 18:23:31 +0500

    Updates

--- lib/presentation/add_salary_range_bottomsheet/models/add_salary_range_model.dart
+++
@@ -1,7 +0,0 @@
-import 'eightynine_item_model.dart';
-import '../../../core/app_export.dart';
-
-class AddSalaryRangeModel {
-  List<EightynineItemModel> eightynineItemList =
-      List.generate(6, (index) => EightynineItemModel());
-}
